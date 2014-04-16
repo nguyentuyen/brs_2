76450dd8f5be7d188e6dfd73aaaa869329c867d8
Chu Anh Tuan <chu.anh.tuan@framgia.com>
2014-04-16 14:14:42 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brs2::Application.config.secret_key_base = '4952ad050f701a004e4c21e62c90d49e36f347438f5600a081df6e0d94c0b1f59b2aac602bf3a29880c7d1834990e1a23d379f38f87d4d799359e5fd9df85432'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Brs2::Application.config.session_store :cookie_store, key: '_brs_2_session'
